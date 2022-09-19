0319464db55495cb26be4cd52f85b1557029b102
Jessie Mason <jmason.p@gmail.com>
2022-09-19 11:45:55 -0700

    Add channel create sample code from Slack API docs - Adds requirements.txt

---
+++ slack_boss.py
@@ -0,0 +1,22 @@
+import logging
+import os
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+logger = logging.getLogger(__name__)
+try:
+    # Call the conversations.create method using the WebClient
+    # conversations_create requires the channels:manage bot scope
+    result = client.conversations_create(
+        # The name of the conversation
+        name="emoji-enthusiasts"
+    )
+    # Log the result which includes information like the ID of the conversation
+    logger.info(result)
+
+except SlackApiError as e:
+    logger.error("Error creating conversation: {}".format(e))
